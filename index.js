c749b314164dd7ca110ca9c24ce8d01d48d410c3
Abdul-Jabar <128983487+abduljabarcodes@users.noreply.github.com>
2024-03-14 09:34:28 +0300

    3rd commit

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 let firstCard = 10;
 let secondCard = 7;
+let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
 let hasBlackjack = false;
 let isAlive = true;
 let message = '';
 let messageEl = document.querySelector("#message-el");
 let sumEl = document.querySelector("#sum-el");
+let cardsEl = document.querySelector("#cards-el");
 
 
 
 
 function startGame() {
+  renderGame();
+}
+
+
+function renderGame() {
+  cardsEl.textContent = `Cards: ${cards[0]} - ${cards[1]}`;
   sumEl.textContent = `sum: ${sum}`;
   if (sum <= 20) {
     message = 'Do you want to draw a new card?';
@@ -25,5 +33,13 @@ function startGame() {
   }
   
   messageEl.textContent = message;
+}
+
+
+function newCard() {
+  let card = 4;
+  sum += card;
+
+  renderGame();
 }
 
